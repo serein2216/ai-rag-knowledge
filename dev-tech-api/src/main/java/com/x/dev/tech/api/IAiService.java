d019cd06109cfc08e17a5922639fdd2180359267
XJH <wyj@2216.com>
2026-06-25 17:50:54 +0800

    第三节

--- dev-tech-api/src/main/java/com/x/dev/tech/api/IAiService.java
+++ dev-tech-api/src/main/java/com/x/dev/tech/api/IAiService.java
@@ -1,4 +1,12 @@
 package com.x.dev.tech.api;
 
+import org.springframework.ai.chat.ChatResponse;
+import reactor.core.publisher.Flux;
+
 public interface IAiService {
+
+    ChatResponse generate(String model, String message);
+
+    Flux<ChatResponse> generateStream(String model, String message);
+
 }
--- dev-tech-trigger/src/main/java/com/x/dev/tech/trigger/http/OllamaController.java
+++ dev-tech-trigger/src/main/java/com/x/dev/tech/trigger/http/OllamaController.java
@@ -1,4 +1,34 @@
 package com.x.dev.tech.trigger.http;
 
-public class OllamaController {
+import com.x.dev.tech.api.IAiService;
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.ai.ollama.api.OllamaOptions;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/v1/ollama/")
+public class OllamaController implements IAiService {
+
+    @Resource
+    private OllamaChatClient chatClient;
+
+    @RequestMapping(value = "generate", method = RequestMethod.GET)
+    @Override
+    public ChatResponse generate(String model, String message) {
+        return chatClient.call(new Prompt(message, OllamaOptions.create().withModel(model)));
+    }
+
+    @RequestMapping(value = "generate_stream", method = RequestMethod.GET)
+    @Override
+    public Flux<ChatResponse> generateStream(String model, String message) {
+        return chatClient.stream(new Prompt(message, OllamaOptions.create().withModel(model)));
+    }
 }
