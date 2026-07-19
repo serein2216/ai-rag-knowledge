6812ed29c6786d29f51ae8af3f51d92342d61896
XJH <wyj@2216.com>
2026-07-19 23:22:49 +0800

    第五节

--- dev-tech-app/src/test/java/com/x/dev/tech/test/RAGTest.java
+++ dev-tech-app/src/test/java/com/x/dev/tech/test/RAGTest.java
@@ -3,13 +3,18 @@ package com.x.dev.tech.test;
 
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.ollama.OllamaChatClient;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.PgVectorStore;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @Slf4j
 @RunWith(SpringRunner.class)
@@ -28,6 +33,19 @@ public class RAGTest {
     @Resource
     private PgVectorStore pgVectorStore;
 
+    @Test
+    public void upload() {
+        TikaDocumentReader reader = new TikaDocumentReader("./data/file.text");
 
+        List<Document> documents = reader.get();
+        List<Document> documentSplitterList = tokenTextSplitter.apply(documents);
+
+        documents.forEach(doc -> doc.getMetadata().put("knowledge", "知识库名称"));
+        documentSplitterList.forEach(doc -> doc.getMetadata().put("knowledge", "知识库名称"));
+
+        pgVectorStore.accept(documentSplitterList);
+
+        log.info("上传完成");
+    }
 
 }
